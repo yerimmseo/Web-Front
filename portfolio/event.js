6d27bc75459da4a3c3299ea45f5fce2935c1d356
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-07-22 23:46:47 +0900

    아이콘 이미지 추가, 경로변경

--- portfolio/event.js
+++ portfolio/event.js
@@ -1,13 +1,19 @@
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 
+const home_btn = document.querySelector('#home');
+const about_btn = document.querySelector('#about');
+const skills_btn = document.querySelector('#skills');
+const projects_btn = document.querySelector('#projects')
+const contact_btn = document.querySelector('#contact');
+
 const hidden_btn = document.querySelector('.hidden-btn');
 const hidden_nav_bar = document.querySelector('.hidden-nav-bar');
 const hidden_nav_bar_body = document.querySelector('.hidden-nav-bar-body');
 const close_btn = document.querySelector('.close-btn');
 
 const github = document.querySelector('#github');
-const instagram = document.querySelector('#instagram')
+const instagram = document.querySelector('#instagram');
 
 function openHiddenNavBar() {
     hidden_nav_bar_body.setAttribute('style', 'display: flex');
@@ -27,6 +33,26 @@ function scroll_off() {
 function scroll_on() {
     body.style.removeProperty('overflow');
 }
+
+contact_btn.addEventListener('click', (e) => {
+    document.querySelector('.contact-body').scrollIntoView(true);
+});
+
+projects_btn.addEventListener('click', (e) => {
+    document.querySelector('.projects-body').scrollIntoView(true);
+});
+
+skills_btn.addEventListener('click', (e) => {
+    document.querySelector('.skills-body').scrollIntoView(true);
+});
+
+about_btn.addEventListener('click', (e) => {
+    document.querySelector('.about-body').scrollIntoView(true);
+});
+
+home_btn.addEventListener('click', (e) => {
+    document.querySelector('.home-body').scrollIntoView(true);
+});
 
 instagram.addEventListener('click', (e) => {
     window.open('https://www.instagram.com/yerimmseo/');

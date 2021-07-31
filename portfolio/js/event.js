66e89766de7701e2b6be361e37a4ec810f4b9e7b
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-07-31 23:58:06 +0900

    home 화면 생성 중

--- portfolio/js/event.js
+++ portfolio/js/event.js
@@ -1,21 +1,70 @@
+// body
 const body = document.querySelector('body');
+// header
 const header = document.querySelector('.header');
-
+// nav-bar
 const home_btn = document.querySelector('#home');
 const about_btn = document.querySelector('#about');
 const skills_btn = document.querySelector('#skills');
 const projects_btn = document.querySelector('#projects')
 const contact_btn = document.querySelector('#contact');
-
+// hidden-nav-bar
 const hidden_btn = document.querySelector('.hidden-btn');
 const hidden_nav_bar = document.querySelector('.hidden-nav-bar');
 const hidden_nav_bar_body = document.querySelector('.hidden-nav-bar-body');
 const close_btn = document.querySelector('.close-btn');
 
+const h_home_btn = document.querySelector('#h-home');
+const h_about_btn = document.querySelector('#h-about');
+const h_skills_btn = document.querySelector('#h-skills');
+const h_projects_btn = document.querySelector('#h-projects')
+const h_contact_btn = document.querySelector('#h-contact');
+// projects
+const project1_btn = document.querySelector('#project01');
+const project2_btn = document.querySelector('#project02');
+const project3_btn = document.querySelector('#project03');
+
+const project_view1 = document.querySelector('.projects-view1');
+const project_view2 = document.querySelector('.projects-view2');
+const project_view3 = document.querySelector('.projects-view3');
+
 const github_team = document.querySelector('.github-team');
+
+// copyright
 const github = document.querySelector('#github');
 const instagram = document.querySelector('#instagram');
 
+// header event
+window.addEventListener('scroll', (e) => {
+    if (pageYOffset >= 10) {
+        header.classList.add('on');
+    } else {
+        header.classList.remove('on');
+    }
+});
+
+// nav-bar event
+home_btn.addEventListener('click', (e) => {
+    document.querySelector('.home-body').scrollIntoView(true);
+});
+
+about_btn.addEventListener('click', (e) => {
+    document.querySelector('.about-body').scrollIntoView(true);
+});
+
+skills_btn.addEventListener('click', (e) => {
+    document.querySelector('.skills-body').scrollIntoView(true);
+});
+
+projects_btn.addEventListener('click', (e) => {
+    document.querySelector('.projects-body').scrollIntoView(true);
+});
+
+contact_btn.addEventListener('click', (e) => {
+    document.querySelector('.contact-body').scrollIntoView(true);
+});
+
+// hidden-nav-bar event
 function openHiddenNavBar() {
     hidden_nav_bar_body.setAttribute('style', 'display: flex');
 }
@@ -27,66 +76,93 @@ function closeHiddenNavBar() {
     hidden_nav_bar_body.setAttribute('style', 'display: none');
 }
 
-function scroll_off() {
+function scrollOff() {
     body.style.overflow = 'hidden';
 }
 
-function scroll_on() {
+function scrollOn() {
     body.style.removeProperty('overflow');
 }
 
-
-contact_btn.addEventListener('click', (e) => {
-    document.querySelector('.contact-body').scrollIntoView(true);
-});
-
-projects_btn.addEventListener('click', (e) => {
-    document.querySelector('.projects-body').scrollIntoView(true);
-});
-
-skills_btn.addEventListener('click', (e) => {
-    document.querySelector('.skills-body').scrollIntoView(true);
-});
-
-about_btn.addEventListener('click', (e) => {
-    document.querySelector('.about-body').scrollIntoView(true);
-});
-
-home_btn.addEventListener('click', (e) => {
-    document.querySelector('.home-body').scrollIntoView(true);
-});
-
-// github_team.addEventListener('click', (e) => {
-//     window.onload.open('https://github.com/TEAM-right/project-cafe.git'); 이거 왜 오류났지
-// })
-
-instagram.addEventListener('click', (e) => {
-    window.onload.open('https://www.instagram.com/yerimmseo/');
-});
-
-github.addEventListener('click', (e) => {
-    window.open('https://github.com/yerimmseo');
-});
-
-window.addEventListener('scroll', (e) => {
-    if (pageYOffset >= 10) {
-        header.classList.add('on');
-    } else {
-        header.classList.remove('on');
-    }
-});
-
 hidden_btn.addEventListener('click', (e) => {
     openHiddenNavBar();
-    scroll_off();
+    scrollOff();
 });
 
 hidden_nav_bar_body.addEventListener('click', (e) => {
     closeHiddenNavBar();
-    scroll_on();
+    scrollOn();
 });
 
 close_btn.addEventListener('click', (e) => {
     closeHiddenNavBar();
-    scroll_on();
+    scrollOn();
+});
+
+h_home_btn.addEventListener('click', (e) => {
+    closeHiddenNavBar();
+    scrollOn();
+    document.querySelector('.home-body').scrollIntoView(true);
+});
+
+h_about_btn.addEventListener('click', (e) => {
+    closeHiddenNavBar();
+    scrollOn();
+    document.querySelector('.about-body').scrollIntoView(true);
+});
+
+h_skills_btn.addEventListener('click', (e) => {
+    closeHiddenNavBar();
+    scrollOn();
+    document.querySelector('.skills-body').scrollIntoView(true);
+});
+
+h_projects_btn.addEventListener('click', (e) => {
+    closeHiddenNavBar();
+    scrollOn();
+    document.querySelector('.projects-body').scrollIntoView(true);
+});
+
+h_contact_btn.addEventListener('click', (e) => {
+    closeHiddenNavBar();
+    scrollOn();
+    document.querySelector('.contact-body').scrollIntoView(true);
+});
+
+// project -> github바로가기 event
+$(document).ready(function(){
+    $('#project01').click(function() {
+        $('.projects-view1').fadeIn(1000);
+        $('.projects-view2').fadeOut(0);
+        $('.projects-view3').fadeOut(0);
+    })
+});
+
+$(document).ready(function(){
+    $('#project02').click(function() {
+        $('.projects-view1').fadeOut(0);
+        $('.projects-view2').fadeIn(1000);
+        $('.projects-view3').fadeOut(0);
+    })
+});
+
+$(document).ready(function(){
+    $('#project03').click(function() {
+        $('.projects-view1').fadeOut(0);
+        $('.projects-view2').fadeOut(0);
+        $('.projects-view3').fadeIn(1000);
+    })
+});
+
+github_team.addEventListener('click', (e) => {
+    window.open('https://github.com/TEAM-right/project-cafe.git');
+});
+
+// copyright -> git, instagram event
+github.addEventListener('click', (e) => {
+    window.open('https://github.com/yerimmseo');
+});
+
+instagram.addEventListener('click', (e) => {
+    window.open('https://www.instagram.com/yerimmseo/');
 });
